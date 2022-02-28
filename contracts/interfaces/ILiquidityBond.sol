1fceb0944a995373f127dfce3fc4535747a3e5fc
Xavier Negron <xaviernegron888@gmail.com>
2022-02-28 09:34:33 -0600

    Create interfaces

---
+++ contracts/interfaces/ILiquidityBond.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+interface ILiquidityBond {
+    // Views
+
+    /**
+     * @dev Calculates the amount of unclaimed rewards the user has available.
+     * @param _account address of the user.
+     * @return (uint256) amount of available unclaimed rewards.
+     */
+    function earned(address _account) external view returns (uint256);
+
+    /**
+     * @dev Returns the total number of tokens purchased.
+     * @return (uint256) total number of tokens purchased.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the number of tokens a user has purchased.
+     * @param _account address of the user.
+     * @return (uint256) number of tokens purchased.
+     */
+    function balanceOf(address _account) external view returns (uint256);
+
+    // Mutative
+
+    /**
+     * @dev Purchases liquidity bonds.
+     * @notice Swaps 1/2 of collateral for TGEN and adds liquidity.
+     * @notice Collateral lost to slippage/fees is returned to the user.
+     * @param _amount amount of collateral to deposit.
+     */
+    function purchase(uint256 _amount) external;
+
+    /**
+     * @dev Claims available rewards for the user.
+     */
+    function getReward() external;
+}
---
+++ contracts/interfaces/IRouter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+interface IRouter {
+    /**
+    * @dev Swaps the given asset for TGEN.
+    * @notice Need to transfer asset to Router contract before calling this function.
+    * @param _asset Address of token to swap from.
+    * @param _amount Number of tokens to swap.
+    */
+    function swapAssetForTGEN(address _asset, uint256 _amount) external;
+
+    /**
+    * @dev Swaps TGEN for the given asset.
+    * @notice Need to transfer TGEN to Router contract before calling this function.
+    * @param _asset Address of token to swap to.
+    * @param _amount Number of TGEN to swap.
+    */
+    function swapTGENForAsset(address _asset, uint256 _amount) external;
+
+    /**
+    * @dev Adds liquidity for asset-TGEN pair.
+    * @notice Need to transfer asset and TGEN to Router contract before calling this function.
+    * @notice Assumes the _amountAsset and _amountTGEN has equal dollar value.
+    * @notice This function is meant to be called from the LiquidityBond contract.
+    * @param _asset Address of other token.
+    * @param _amountAsset Amount of other token to add.
+    * @param _amountTGEN Amount of TGEN to add.
+    */
+    function addLiquidity(address _asset, uint256 _amountAsset, uint256 _amountTGEN) external;
+}
