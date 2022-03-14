149da9c008e35fd4f75057183fbf033e12d2c6c1
Xavier Negron <xaviernegron888@gmail.com>
2022-03-14 14:02:49 -0500

    Set up Router test

---
+++ test/Router.test.js
@@ -0,0 +1,94 @@
+const { expect } = require("chai");
+const { parseEther } = require("@ethersproject/units");
+
+describe("Router", () => {
+  let deployer;
+  let otherUser;
+
+  let tradegenToken;
+  let tradegenTokenAddress;
+  let mockCELO;
+  let mockCELOAddress;
+  let TestTokenFactory;
+
+  let ubeswapFactory;
+  let ubeswapFactoryAddress;
+  let UbeswapFactoryFactory;
+
+  let ubeswapRouter;
+  let ubeswapRouterAddress;
+  let UbeswapRouterFactory;
+
+  let pairData;
+  let pairDataAddress;
+  let PairDataFactory;
+  
+  let pathManager;
+  let pathManagerAddress;
+  let PathManagerFactory;
+
+  let router;
+  let routerAddress;
+  let RouterFactory;
+  
+  before(async () => {
+    const signers = await ethers.getSigners();
+    deployer = signers[0];
+    otherUser = signers[1];
+
+    TestTokenFactory = await ethers.getContractFactory('TestTokenERC20');
+    UbeswapFactoryFactory = await ethers.getContractFactory('UniswapV2Factory');
+    UbeswapRouterFactory = await ethers.getContractFactory('UniswapV2Router02');
+    PairDataFactory = await ethers.getContractFactory('PairData');
+    PathManagerFactory = await ethers.getContractFactory('UbeswapPathManager');
+    RouterFactory = await ethers.getContractFactory('Router');
+
+    tradegenToken = await TestTokenFactory.deploy("Test TGEN", "TGEN");
+    await tradegenToken.deployed();
+    tradegenTokenAddress = tradegenToken.address;
+
+    mockCELO = await TestTokenFactory.deploy("Test CELO", "CELO");
+    await mockCELO.deployed();
+    mockCELOAddress = mockCELO.address;
+
+    ubeswapFactory = await UbeswapFactoryFactory.deploy(deployer.address);
+    await ubeswapFactory.deployed();
+    ubeswapFactoryAddress = ubeswapFactory.address;
+
+    ubeswapRouter = await UbeswapRouterFactory.deploy(ubeswapFactoryAddress);
+    await ubeswapRouter.deployed();
+    ubeswapRouterAddress = ubeswapRouter.address;
+
+    pairData = await PairDataFactory.deploy();
+    await pairData.deployed();
+    pairDataAddress = pairData.address;
+
+    pathManager = await PathManagerFactory.deploy();
+    await pathManager.deployed();
+    pathManagerAddress = pathManager.address;
+  });
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners();
+    deployer = signers[0];
+    otherUser = signers[1];
+
+    router = await RouterFactory.deploy(pathManagerAddress, ubeswapRouterAddress, tradegenTokenAddress);
+    await router.deployed();
+    routerAddress = router.address;
+
+    // Create TGEN-CELO pair and supply seed liquidity.
+    let tx = await ubeswapRouter.addLiquidity(tradegenTokenAddress, mockCELOAddress, parseEther("1000"), parseEther("1000"), 0, 0);
+    await tx.wait();
+  });
+  
+  describe("#addLiquidity", () => {
+    it("unbalanced amounts", async () => {
+        let tx = pathManager.connect(otherUser).setPath(testTokenAddress1, testTokenAddress2, [testTokenAddress1, testTokenAddress2]);
+        await expect(tx).to.be.reverted;
+
+        let path = pathManager.getPath(testTokenAddress1, testTokenAddress2);
+        await expect(path).to.be.reverted;
+    });
+  });
+});
