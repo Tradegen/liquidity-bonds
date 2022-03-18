ef26b82561998a2b1b02bf60fa9c74063006cba4
Xavier Negron <xaviernegron888@gmail.com>
2022-03-18 12:48:39 -0500

    Create TestExecutionPrice.sol

---
+++ contracts/test/TestExecutionPrice.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+import "../ExecutionPrice.sol";
+
+contract TestExecutionPrice is ExecutionPrice {
+    constructor(address _TGEN, address _bondToken, address _marketplace, address _xTGEN)
+        ExecutionPrice(_TGEN, _bondToken, _marketplace, _xTGEN)
+    {
+    }
+
+    function setStartIndex(uint256 _index) external {
+        startIndex = _index;
+    }
+
+    function setEndIndex(uint256 _index) external {
+        endIndex = _index;
+    }
+
+    function setIsBuyQueue(bool _isBuyQueue) external {
+        isBuyQueue = _isBuyQueue;
+    }
+
+    function setNumberOfTokensAvailable(uint256 _amount) external {
+        numberOfTokensAvailable = _amount;
+    }
+
+    function setOrderIndex(address _user, uint256 _index) external {
+        orderIndex[_user] = _index;
+    }
+
+    function setOrder(uint256 _index, address _user, uint256 _quantity, uint256 _amountFilled) external {
+        orderBook[_index] = Order({
+            user: _user,
+            quantity: _quantity,
+            amountFilled: _amountFilled
+        });
+    }
+}
